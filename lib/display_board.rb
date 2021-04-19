589358f7e524b54b6f091efe7973926747d66450
Rishav Maskey <rishav.maskey@hotmail.com>
2021-04-19 05:25:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_value val
+  " #{val} "
+end
+
+def display_board board
+  _horizontal_separator = "-----------"
+  
+  puts "#{display_value }|#{display_value}|#{display_value}"
+  puts _horizontal_separator
+  puts "#{display_value }|#{display_value}|#{display_value}"
+  puts _horizontal_separator
+  puts "#{display_value }|#{display_value}|#{display_value}"
+
+end
